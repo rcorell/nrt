6d0db3f29c40413cc2d6164faa973b8b05770879
Rob Corell <rob@robcorell.com>
2021-04-28 18:53:34 -0500

    Add import path aliases

--- client/src/components/signup/Signup.tsx
+++ client/src/components/signup/Signup.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { Button, Form, FormControlProps } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
-import * as signupActions from '../../redux/signup/actions';
-import { State } from '../../redux/state.types';
-import { AuthError, AuthForm, FormContainer } from '../../styles';
+import { AuthError, AuthForm, FormContainer } from '@app/styles';
+import * as signupActions from '@redux/signup/actions';
+import { State } from '@redux/state.types';
 
 interface SignupStateProps {
     dummy: string;
