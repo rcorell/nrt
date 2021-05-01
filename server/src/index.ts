36fd88113170b1c331a9bf3171d7b10fd536c3e8
Rob Corell <rob@robcorell.com>
2021-05-01 01:48:55 -0500

    Add standard scalars

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 import { ApolloServer } from 'apollo-server';
 import * as fs from 'fs';
+import { typeDefs as scalarTypeDefs, resolvers as scalarResolvers } from 'graphql-scalars';
 import * as path from 'path';
 
 import * as Mutation from './resolvers/mutations';
 import * as Query from './resolvers/queries';
 import * as Topic from './resolvers/topic';
 import * as User from './resolvers/user';
-import { DateTimeScalar, getUserId } from './utils';
+import { getUserId } from './utils';
 
 const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error']
 });
 
-const resolvers = {
-    DateTime: DateTimeScalar,
-    Mutation,
-    Query,
-    Topic,
-    User
-};
+const coreTypeDefs = fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8');
 
 const server = new ApolloServer({
     context: ({ req }) => {
@@ -29,8 +24,14 @@ const server = new ApolloServer({
             userId: req && req.headers.authorization ? getUserId(req) : null
         };
     },
-    resolvers,
-    typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8')
+    resolvers: {
+        Mutation,
+        Query,
+        Topic,
+        User,
+        ...scalarResolvers
+    },
+    typeDefs: [coreTypeDefs].concat(scalarTypeDefs)
 });
 
 server.listen().then((response) => console.log(`Server is running on ${response.url}\n`, JSON.stringify(response)));
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,4 +1,3 @@
-import { GraphQLScalarType, Kind } from 'graphql';
 import jsonwebtoken from 'jsonwebtoken';
 
 export const APP_SECRET = 'INSECURE stub for app secret';
@@ -30,20 +29,3 @@ export const getUserId = (req, authToken = null) => {
 
     throw new Error('Not authenticated');
 };
-
-export const DateTimeScalar = new GraphQLScalarType({
-    description: 'DateTime custom scalar type',
-    name: 'DateTime',
-    parseLiteral(ast) {
-        if (ast.kind === Kind.INT) {
-            return new Date(parseInt(ast.value, 10));
-        }
-        return null;
-    },
-    parseValue(value) {
-        return new Date(value);
-    },
-    serialize(value) {
-        return value.getTime();
-    }
-});
