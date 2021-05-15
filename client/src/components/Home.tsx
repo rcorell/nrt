83d1c875a6132bafd0581f31beaa0e3b034d32a9
Rob Corell <rob@robcorell.com>
2021-05-15 12:31:04 -0500

    Refactor home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,33 +1,9 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
 
 import { setBrowserTitle } from 'src/utils';
-import { State } from 'src/redux/state.types';
 
-interface HomeStateProps {
-    dummy: string;
-}
+export const Home: React.FC = () => {
+    setBrowserTitle('Home');
 
-// interface HomeDispatchProps {}
-
-export type HomeComponentProps = HomeStateProps; // & HomeDispatchProps;
-
-export class HomeComponent extends React.Component<HomeComponentProps> {
-    public constructor(props: HomeComponentProps) {
-        super(props);
-    }
-
-    componentDidMount() {
-        setBrowserTitle('Home');
-    }
-
-    render() {
-        return <h1>Home</h1>;
-    }
-}
-
-export const mapStateToProps = (_state: State) => ({});
-
-export const mapDispatchToProps = (_dispatch: Function) => ({});
-
-export const Home = connect(mapStateToProps, mapDispatchToProps)(HomeComponent);
+    return <h1>Home</h1>;
+};
