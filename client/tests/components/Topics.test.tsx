9dd1ad60858dd64c418cb7f6cb6639e06e1fa443
Rob Corell <rob@robcorell.com>
2021-05-24 00:20:57 -0500

    Some tests for Topics component

--- client/tests/components/Topics.test.tsx
+++ client/tests/components/Topics.test.tsx
@@ -1,7 +1,71 @@
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import * as React from 'react';
+
+import { FetchTopicsQuery } from 'src/api/__generated__/FetchTopicsQuery';
+import { fetchTopicsQueryString } from 'src/api/api';
 import { Topics } from 'src/components/Topics';
 
 describe('Topics', () => {
-    it('matches snapshot', () => {
-        global.expectComponentToMatchSnapshot(Topics);
+    const renderTopics = (results: any = null) => {
+        const mocks: MockedResponse[] = [
+            {
+                request: {
+                    query: fetchTopicsQueryString,
+                    variables: {}
+                }
+            }
+        ];
+
+        if (results?.data) {
+            mocks[0].result = {
+                data: results.data
+            };
+        } else if (results?.error) {
+            mocks[0].error = results.error;
+        }
+
+        return render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Topics />
+            </MockedProvider>
+        );
+    };
+
+    describe('snapshots', () => {
+        it('loading', () => {
+            expect(renderTopics()).toMatchSnapshot();
+        });
+
+        it('success', async () => {
+            const data: FetchTopicsQuery = {
+                topics: [{ __typename: 'Topic', description: 'test-description', title: 'test-title' }]
+            };
+
+            renderTopics({ data });
+
+            await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+            // Material UI devs literally hate snapshots
+            expect(screen.queryByText('test-description')).toBeInTheDocument();
+            expect(screen.queryByText('test-title')).toBeInTheDocument();
+        });
+
+        it('network error', async () => {
+            const networkError = new Error('Network-error');
+
+            const topicsRender = renderTopics({ error: networkError });
+
+            await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+            expect(screen.queryByText('Loading...')).toBeNull();
+            expect(screen.queryByText('No results')).toBeNull();
+
+            expect(topicsRender).toMatchSnapshot();
+        });
+
+        // GraphQL Error
+
+        // "No results" (unlikely malformed response)
     });
 });
