b600436291af28861c7d33f41cb741a5033adb7a
Ezequiel <ezequiel.zacca@fivestars.com>
2021-08-06 16:39:49 -0300

    added loading state to mutations

--- client/src/components/Signup/Signup.hook.ts
+++ client/src/components/Signup/Signup.hook.ts
@@ -5,7 +5,7 @@ import { GlobalContext } from '../GlobalContextProvider';
 
 export const useSignup = () => {
     const { setAuthenticated } = useContext(GlobalContext);
-    const [callMutation, { error }] = useSignupMutation({
+    const [callMutation, { error, loading }] = useSignupMutation({
         onCompleted: (result) => {
             if (result?.signup) {
                 localStorage.setItem('token', result.signup.token!);
@@ -28,5 +28,5 @@ export const useSignup = () => {
         });
     };
 
-    return { error, signup };
+    return { error, loading, signup };
 };
