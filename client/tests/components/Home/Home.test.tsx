c25b017b0e176850796173aeca20d264b23b823f
Rob Corell <rob@robcorell.com>
2021-08-06 15:09:16 -0500

    Add missing Home coverage

--- client/tests/components/Home/Home.test.tsx
+++ client/tests/components/Home/Home.test.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { FetchUserWithGroupTopicsQuery } from 'src/api/__generated__/types';
 import { Home } from 'src/components/Home/Home';
+import { render } from '@testing-library/react';
 
 import { TEST_USER_WITH_GROUPS } from 'tests/mocks/userMocks';
 import { createComponentMocks, testComponent } from 'tests/testHelpers';
@@ -8,8 +10,19 @@ const mocks = createComponentMocks<FetchUserWithGroupTopicsQuery>({
     user: TEST_USER_WITH_GROUPS
 });
 
+mocks.noGroups = {
+    user: {
+        groups: []
+    }
+};
+
 jest.mock('src/components/Home/Home.hook', () => ({
     useUserWithGroupTopics: () => mocks[mocks.mockState]
 }));
 
 testComponent(Home, mocks);
+
+it('shows "join a group" if user has not groups', () => {
+    mocks.mockState = 'noGroups';
+    expect(render(<Home />).container).toMatchSnapshot();
+});
